fc00e3d22735057f0304d50278c1f4002219419a
acham1 <me@alanch.am>
2019-02-25 08:37:28 +0000

    Stub exec family calls

---
+++ homework/process-api/4_fork_exec.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tryexecl() {
+}
+
+void tryexeclp() {
+}
+
+void tryexecle() {
+}
+
+void tryexecv() {
+}
+
+void tryexecvp() {
+}
+
+void tryexecvpe() {
+}
+
+int main(int argc, char* argv[]) {
+  tryexecl();
+  tryexeclp();
+  tryexecle();
+  tryexecv();
+  tryexecvp();
+  tryexecvpe();
+  return EXIT_SUCCESS;
+}
