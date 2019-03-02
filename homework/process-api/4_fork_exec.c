d5a5d0b0354a283a93d5ff14777fee8f0a4f4b25
acham1 <me@alanch.am>
2019-03-02 08:50:54 +0000

    Finish demonstrating all of exec family

--- homework/process-api/4_fork_exec.c
+++ homework/process-api/4_fork_exec.c
@@ -1,10 +1,13 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <limits.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
+// l := list arguments variadically
 void tryexecl(char* cwd) {
   if (fork()) {
     printf("Trying execl\n");
@@ -15,9 +18,20 @@ void tryexecl(char* cwd) {
   }
 }
 
-void tryexeclp() {
+// l ...
+// p := search in environment PATH
+void tryexeclp(char* cwd) {
+  if (fork()) {
+    printf("Trying execlp\n");
+    execlp("ls", cwd, NULL);
+    exit(EXIT_SUCCESS);
+  } else {
+    wait(NULL);
+  }
 }
 
+// l ...
+// e := give environment as array of K=V strings
 void tryexecle(char* cwd) {
   if (fork()) {
     printf("Trying execle\n");
@@ -28,24 +42,56 @@ void tryexecle(char* cwd) {
   }
 }
 
-void tryexecv() {
+// v := arguments as array of strings
+void tryexecv(char* cwd) {
+  if (fork()) {
+    char* args[] = {cwd, NULL};
+    printf("Trying execv\n");
+    execv("/bin/ls", args);
+    exit(EXIT_SUCCESS);
+  } else {
+    wait(NULL);
+  }
 }
 
-void tryexecvp() {
+// v ...
+// p ...
+void tryexecvp(char* cwd) {
+  if (fork()) {
+    char* args[] = {cwd, NULL};
+    printf("Trying execvp\n");
+    execvp("ls", args);
+    exit(EXIT_SUCCESS);
+  } else {
+    wait(NULL);
+  }
 }
 
-void tryexecvpe() {
+// v ...
+// p ...
+// e ...
+void tryexecvpe(char* cwd) {
+  if (fork()) {
+    char* args[] = {cwd, NULL};
+    printf("Trying execvpe\n");
+    execvpe("ls", args, (char* const*) {NULL});
+    exit(EXIT_SUCCESS);
+  } else {
+    wait(NULL);
+  }
 }
 
 int main(int argc, char* argv[]) {
+  int status;
+  pid_t wpid;
   char cwd[PATH_MAX];
   getcwd(cwd, sizeof(cwd));
   
   tryexecl(cwd);
-  tryexeclp();
+  tryexeclp(cwd);
   tryexecle(cwd);
-  tryexecv();
-  tryexecvp();
-  tryexecvpe();
+  tryexecv(cwd);
+  tryexecvp(cwd);
+  tryexecvpe(cwd);
   return EXIT_SUCCESS;
 }
